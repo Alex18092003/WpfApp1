43140d4e4768cddf186e1edca8420fe39302a7f9
Саша Лебедева <sashyla1809@gmail.com>
2022-09-27 14:09:40 +0300

    Создана обработка количества дней в каждом месяце

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -23,6 +23,8 @@ namespace WpfApp1
         public MainWindow()
         {
             InitializeComponent();
+            this.TBzn.PreviewTextInput += new TextCompositionEventHandler(TBzn_PreviewTextInput);
+            this.TBvost.PreviewTextInput += new TextCompositionEventHandler(TBzn_PreviewTextInput);
         }
 
         private void CBGoroscop_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -31,19 +33,148 @@ namespace WpfApp1
             {
                 case 0:
                     TBOtvet.Text = "";
+                    TBzn.Text = "";
+                    CBMes.Text = "";
                     TBzn.Visibility = Visibility.Visible;
                     CBMes.Visibility = Visibility.Visible;
                     TBvost.Visibility = Visibility.Collapsed;
                     break;
                 case 1:
                     TBOtvet.Text = "";
+                    TBvost.Text = "";
                     TBzn.Visibility = Visibility.Collapsed;
                     CBMes.Visibility = Visibility.Collapsed;
                     TBvost.Visibility = Visibility.Visible;
                     break;
+            }
+        }
 
+        private void CBMes_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {         
+            switch (CBMes.SelectedIndex)
+            {
+                case 0:
+                        if (int.Parse(TBzn.Text) > 31 || int.Parse(TBzn.Text) < 1)
+                        {
+                            MessageBox.Show("В январе только 31 день.\nПовторите ввод");
+                            TBzn.Text = "";
+                        }
+                        else
+                        MessageBox.Show("Вы успешно ввели данные");
+                    break;
+                case 1:
+                    if (int.Parse(TBzn.Text) > 28 || int.Parse(TBzn.Text) < 1)
+                    {
+                        MessageBox.Show("В феврале только 28 день.\nПовторите ввод");
+                        TBzn.Text = "";
+                    }
+                    else
+                        MessageBox.Show("Вы успешно ввели данные");
+                    break;
+                case 2:
+                    if (int.Parse(TBzn.Text) > 31 || int.Parse(TBzn.Text) < 1)
+                    {
+                        MessageBox.Show("В марте только 31 день.\nПовторите ввод");
+                        TBzn.Text = "";
+                    }
+                    else
+                        MessageBox.Show("Вы успешно ввели данные");
+                    break;
+                case 3:
+                    if (int.Parse(TBzn.Text) > 30 || int.Parse(TBzn.Text) < 1)
+                    {
+                        MessageBox.Show("В апреле только 30 день.\nПовторите ввод");
+                        TBzn.Text = "";
+                    }
+                    else
+                        MessageBox.Show("Вы успешно ввели данные");
+                    break;
+                case 4:
+                    if (int.Parse(TBzn.Text) > 31 || int.Parse(TBzn.Text) < 1)
+                    {
+                        MessageBox.Show("В мае только 31 день.\nПовторите ввод");
+                        TBzn.Text = "";
+                    }
+                    else
+                        MessageBox.Show("Вы успешно ввели данные");
+                    break;
+                case 5:
+                    if (int.Parse(TBzn.Text) > 30 || int.Parse(TBzn.Text) < 1)
+                    {
+                        MessageBox.Show("В июне только 30 день.\nПовторите ввод");
+                        TBzn.Text = "";
+                    }
+                    else
+                        MessageBox.Show("Вы успешно ввели данные");
+                    break;
+                case 6:
+                    if (int.Parse(TBzn.Text) > 31 || int.Parse(TBzn.Text) < 1)
+                    {
+                        MessageBox.Show("В июле только 31 день.\nПовторите ввод");
+                        TBzn.Text = "";
+                    }
+                    else
+                        MessageBox.Show("Вы успешно ввели данные");
+                    break;
+                case 7:
+                    if (int.Parse(TBzn.Text) > 31 || int.Parse(TBzn.Text) < 1)
+                    {
+                        MessageBox.Show("В августе только 31 день.\nПовторите ввод");
+                        TBzn.Text = "";
+                    }
+                    else
+                        MessageBox.Show("Вы успешно ввели данные");
+                    break;
+                case 8:
+                    if (int.Parse(TBzn.Text) > 30 || int.Parse(TBzn.Text) < 1)
+                    {
+                        MessageBox.Show("В сентябре только 30 день.\nПовторите ввод");
+                        TBzn.Text = "";
+                    }
+                    else
+                        MessageBox.Show("Вы успешно ввели данные");
+                    break;
+                case 9:
+                    if (int.Parse(TBzn.Text) > 31 || int.Parse(TBzn.Text) < 1)
+                    {
+                        MessageBox.Show("В октябре только 31 день.\nПовторите ввод");
+                        TBzn.Text = "";
+                    }
+                    else
+                        MessageBox.Show("Вы успешно ввели данные");
+                    break;
+                case 10:
+                    if (int.Parse(TBzn.Text) > 30 || int.Parse(TBzn.Text) < 1)
+                    {
+                        MessageBox.Show("В ноябре только 30 день.\nПовторите ввод");
+                        TBzn.Text = "";
+                    }
+                    else
+                        MessageBox.Show("Вы успешно ввели данные");
+                    break;
+                case 11:
+                    if (int.Parse(TBzn.Text) > 31 || int.Parse(TBzn.Text) < 1)
+                    {
+                        MessageBox.Show("В декабре только 31 день.\nПовторите ввод");
+                        TBzn.Text = "";
+                    }
+                    else
+                        MessageBox.Show("Вы успешно ввели данные");
+                    break;
+              
 
             }
+        }
+
+      
+        private void TBzn_PreviewTextInput(object sender, TextCompositionEventArgs e) //Ограничение на ввод только чисел 
+        {
+            if (!Char.IsDigit(e.Text, 0)) e.Handled = true;
+        }
+    
+        private void TBvost_PreviewTextInput(object sender, TextCompositionEventArgs e)//Ограничение на ввод только чисел 
+        {
+            if (!Char.IsDigit(e.Text, 0)) e.Handled = true;
         }
     }
 }
