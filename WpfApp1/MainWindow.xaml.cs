f34caa2a55b75c40b0121bf20cc8a530e3085c59
AleksandraLebedeva <sashyla1809@gmail.com>
2022-09-29 19:01:26 +0300

    Полный функционал знаков зодиака

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -50,121 +50,114 @@ namespace WpfApp1
         }
 
         private void CBMes_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {         
-            switch (CBMes.SelectedIndex)
+        {
+            if (TBzn.Text == "")
+            { }
+            else
             {
-                case 0:
+                switch (CBMes.SelectedIndex)
+                {
+                    case 0:
                         if (int.Parse(TBzn.Text) > 31 || int.Parse(TBzn.Text) < 1)
                         {
                             MessageBox.Show("В январе только 31 день.\nПовторите ввод");
                             TBzn.Text = "";
                         }
-                        else
-                        MessageBox.Show("Вы успешно ввели данные");
-                    break;
-                case 1:
-                    if (int.Parse(TBzn.Text) > 28 || int.Parse(TBzn.Text) < 1)
-                    {
-                        MessageBox.Show("В феврале только 28 день.\nПовторите ввод");
-                        TBzn.Text = "";
-                    }
-                    else
-                        MessageBox.Show("Вы успешно ввели данные");
-                    break;
-                case 2:
-                    if (int.Parse(TBzn.Text) > 31 || int.Parse(TBzn.Text) < 1)
-                    {
-                        MessageBox.Show("В марте только 31 день.\nПовторите ввод");
-                        TBzn.Text = "";
-                    }
-                    else
-                        MessageBox.Show("Вы успешно ввели данные");
-                    break;
-                case 3:
-                    if (int.Parse(TBzn.Text) > 30 || int.Parse(TBzn.Text) < 1)
-                    {
-                        MessageBox.Show("В апреле только 30 день.\nПовторите ввод");
-                        TBzn.Text = "";
-                    }
-                    else
-                        MessageBox.Show("Вы успешно ввели данные");
-                    break;
-                case 4:
-                    if (int.Parse(TBzn.Text) > 31 || int.Parse(TBzn.Text) < 1)
-                    {
-                        MessageBox.Show("В мае только 31 день.\nПовторите ввод");
-                        TBzn.Text = "";
-                    }
-                    else
-                        MessageBox.Show("Вы успешно ввели данные");
-                    break;
-                case 5:
-                    if (int.Parse(TBzn.Text) > 30 || int.Parse(TBzn.Text) < 1)
-                    {
-                        MessageBox.Show("В июне только 30 день.\nПовторите ввод");
-                        TBzn.Text = "";
-                    }
-                    else
-                        MessageBox.Show("Вы успешно ввели данные");
-                    break;
-                case 6:
-                    if (int.Parse(TBzn.Text) > 31 || int.Parse(TBzn.Text) < 1)
-                    {
-                        MessageBox.Show("В июле только 31 день.\nПовторите ввод");
-                        TBzn.Text = "";
-                    }
-                    else
-                        MessageBox.Show("Вы успешно ввели данные");
-                    break;
-                case 7:
-                    if (int.Parse(TBzn.Text) > 31 || int.Parse(TBzn.Text) < 1)
-                    {
-                        MessageBox.Show("В августе только 31 день.\nПовторите ввод");
-                        TBzn.Text = "";
-                    }
-                    else
-                        MessageBox.Show("Вы успешно ввели данные");
-                    break;
-                case 8:
-                    if (int.Parse(TBzn.Text) > 30 || int.Parse(TBzn.Text) < 1)
-                    {
-                        MessageBox.Show("В сентябре только 30 день.\nПовторите ввод");
-                        TBzn.Text = "";
-                    }
-                    else
-                        MessageBox.Show("Вы успешно ввели данные");
-                    break;
-                case 9:
-                    if (int.Parse(TBzn.Text) > 31 || int.Parse(TBzn.Text) < 1)
-                    {
-                        MessageBox.Show("В октябре только 31 день.\nПовторите ввод");
-                        TBzn.Text = "";
-                    }
-                    else
-                        MessageBox.Show("Вы успешно ввели данные");
-                    break;
-                case 10:
-                    if (int.Parse(TBzn.Text) > 30 || int.Parse(TBzn.Text) < 1)
-                    {
-                        MessageBox.Show("В ноябре только 30 день.\nПовторите ввод");
-                        TBzn.Text = "";
-                    }
-                    else
-                        MessageBox.Show("Вы успешно ввели данные");
-                    break;
-                case 11:
-                    if (int.Parse(TBzn.Text) > 31 || int.Parse(TBzn.Text) < 1)
-                    {
-                        MessageBox.Show("В декабре только 31 день.\nПовторите ввод");
-                        TBzn.Text = "";
-                    }
-                    else
-                        MessageBox.Show("Вы успешно ввели данные");
-                    break;
-                case 12:
-                    break;
+                       
+                        break;
+                    case 1:
+                        if (int.Parse(TBzn.Text) > 28 || int.Parse(TBzn.Text) < 1)
+                        {
+                            MessageBox.Show("В феврале только 28 день.\nПовторите ввод");
+                            TBzn.Text = "";
+                        }
+                       
+                        break;
+                    case 2:
+                        if (int.Parse(TBzn.Text) > 31 || int.Parse(TBzn.Text) < 1)
+                        {
+                            MessageBox.Show("В марте только 31 день.\nПовторите ввод");
+                            TBzn.Text = "";
+                        }
+                        
+                        break;
+                    case 3:
+                        if (int.Parse(TBzn.Text) > 30 || int.Parse(TBzn.Text) < 1)
+                        {
+                            MessageBox.Show("В апреле только 30 день.\nПовторите ввод");
+                            TBzn.Text = "";
+                        }
+                       
+                        break;
+                    case 4:
+                        if (int.Parse(TBzn.Text) > 31 || int.Parse(TBzn.Text) < 1)
+                        {
+                            MessageBox.Show("В мае только 31 день.\nПовторите ввод");
+                            TBzn.Text = "";
+                        }
+                        
+                        break;
+                    case 5:
+                        if (int.Parse(TBzn.Text) > 30 || int.Parse(TBzn.Text) < 1)
+                        {
+                            MessageBox.Show("В июне только 30 день.\nПовторите ввод");
+                            TBzn.Text = "";
+                        }
+                       
+                        break;
+                    case 6:
+                        if (int.Parse(TBzn.Text) > 31 || int.Parse(TBzn.Text) < 1)
+                        {
+                            MessageBox.Show("В июле только 31 день.\nПовторите ввод");
+                            TBzn.Text = "";
+                        }
+                        
+                        break;
+                    case 7:
+                        if (int.Parse(TBzn.Text) > 31 || int.Parse(TBzn.Text) < 1)
+                        {
+                            MessageBox.Show("В августе только 31 день.\nПовторите ввод");
+                            TBzn.Text = "";
+                        }
+                       
+                        break;
+                    case 8:
+                        if (int.Parse(TBzn.Text) > 30 || int.Parse(TBzn.Text) < 1)
+                        {
+                            MessageBox.Show("В сентябре только 30 день.\nПовторите ввод");
+                            TBzn.Text = "";
+                        }
+                        
+                        break;
+                    case 9:
+                        if (int.Parse(TBzn.Text) > 31 || int.Parse(TBzn.Text) < 1)
+                        {
+                            MessageBox.Show("В октябре только 31 день.\nПовторите ввод");
+                            TBzn.Text = "";
+                        }
+                       
+                        break;
+                    case 10:
+                        if (int.Parse(TBzn.Text) > 30 || int.Parse(TBzn.Text) < 1)
+                        {
+                            MessageBox.Show("В ноябре только 30 день.\nПовторите ввод");
+                            TBzn.Text = "";
+                        }
+                        
+                        break;
+                    case 11:
+                        if (int.Parse(TBzn.Text) > 31 || int.Parse(TBzn.Text) < 1)
+                        {
+                            MessageBox.Show("В декабре только 31 день.\nПовторите ввод");
+                            TBzn.Text = "";
+                        }
+                       
+                        break;
+                    case 12:
+                        break;
 
 
+                }
             }
         }
 
@@ -260,27 +253,46 @@ namespace WpfApp1
                 }
                 else
                 {
-                    MessageBox.Show("Заполните поля");
+                    MessageBox.Show("Заполните все поля");
                 }
             }
             else if (CBGoroscop.SelectedIndex == 1)
             {
                 if (TBvost.Text != "")
                 {
-                    if ((int.Parse(TBzn.Text) >= 19 && int.Parse(TBzn.Text) <= 28 && CBMes.SelectedIndex == 1) || (int.Parse(TBzn.Text) >= 1 && int.Parse(TBzn.Text) <= 20 && CBMes.SelectedIndex == 2))
+                    if (int.Parse(TBvost.Text) >= 2023 || int.Parse(TBvost.Text) <= 1923)
                     {
-                        TBOtvet.Text = ($"Дата рождения {TBzn.Text} {CBMes.Text}\nВаш знак зодиака Рыбы");
-                        TBvost.Text = "";
+                        MessageBox.Show("Введите корректно год рождения");
                     }
+                    else
+                    {
+                        if (int.Parse(TBvost.Text) == 1924 || int.Parse(TBvost.Text) == 1936 || int.Parse(TBvost.Text) == 1948 || int.Parse(TBvost.Text) == 1960 || int.Parse(TBvost.Text) == 1972 || int.Parse(TBvost.Text) == 1984 || int.Parse(TBvost.Text) == 1996 || int.Parse(TBvost.Text) == 2008 || int.Parse(TBvost.Text) == 2020)
+                        {
+                            TBOtvet.Text = ($"Год рождения {TBvost.Text} \nВаш восточный знак - Крыса ");
+                        }
+                        else if(int.Parse(TBvost.Text) == 1925 || int.Parse(TBvost.Text) == 1937 || int.Parse(TBvost.Text) == 1949 || int.Parse(TBvost.Text) == 1961 || int.Parse(TBvost.Text) == 1973 || int.Parse(TBvost.Text) == 1985 || int.Parse(TBvost.Text) == 1997 || int.Parse(TBvost.Text) == 2009 || int.Parse(TBvost.Text) == 2021)
+                        {
+                            TBOtvet.Text = ($"Год рождения {TBvost.Text} \nВаш восточный знак - Бык ");
+                        }
+                        else if (int.Parse(TBvost.Text) == 1926 || int.Parse(TBvost.Text) == 1938 || int.Parse(TBvost.Text) == 1950 || int.Parse(TBvost.Text) == 1962 || int.Parse(TBvost.Text) == 1974 || int.Parse(TBvost.Text) == 1986 || int.Parse(TBvost.Text) == 1998 || int.Parse(TBvost.Text) == 2010 || int.Parse(TBvost.Text) == 2022)
+                        {
+                            TBOtvet.Text = ($"Год рождения {TBvost.Text} \nВаш восточный знак - Тигр ");
+                        }
+                        else if (int.Parse(TBvost.Text) == 1927 || int.Parse(TBvost.Text) == 1939 || int.Parse(TBvost.Text) == 1951 || int.Parse(TBvost.Text) == 1963 || int.Parse(TBvost.Text) == 1975 || int.Parse(TBvost.Text) == 1987 || int.Parse(TBvost.Text) == 1999 || int.Parse(TBvost.Text) == 2011)
+                        {
+                            TBOtvet.Text = ($"Год рождения {TBvost.Text} \nВаш восточный знак - Тигр ");
+                        }
+                    }
+
                 }
                 else
                 {
-                    MessageBox.Show("Заполните поля");
+                    MessageBox.Show("Заполните поле");
                 }
             }
             else
             {              
-                    MessageBox.Show("Выберите");             
+                    MessageBox.Show("Выберите что хотите узнать");             
             }
 
         }
